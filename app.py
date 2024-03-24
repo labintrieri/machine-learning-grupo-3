bfbde4a45ad9d04a79944aaec5506aa79a635108
labintrieri <144967648+labintrieri@users.noreply.github.com>
2024-03-24 10:37:26 -0300

    Atualização

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
 from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__) # Cria uma instância do Flask. 
 
-
 # Define rota da página principal
 @app.route('/')
 def index():
     return render_template('index.html') # Renderiza o template index.html, localizado na pasta templates
 
 @app.route("/infos")
-def choraviola():
- return "Olá! Você pode me contatar das seguintes formas:"
+def infos():
+ return render_template('infos.html')
 
 @app.route("/projetos")
-def choraviola():
- return "Este é meu GitHub: "
+def projetos():
+ return render_template('projetos.html')
+
+@app.route("/publicacoes")
+def publicacoes():
+ return render_template('publicacoes.html')
 
 if __name__ == '__main__':
   app.run(port=5000, debug=True) # Inicia o servidor na porta 5000. "Debug" é uma configuração para facilitar o desenvolvimento.
